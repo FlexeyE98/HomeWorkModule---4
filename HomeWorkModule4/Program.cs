7cb19b24c3987c3c3c0b1777d1134cf1de9d1ca1
Alexey Emelchikov <aleksemelchikov@gmail.com>
2023-04-06 16:04:11 +0300

    Работа с условиями

---
+++ HomeWorkModule4/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace HomeWorkModule4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Введите ваш любимый цвет: ");
+            string color = Console.ReadLine();
+
+            if (color == "red")
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+            }
+            else if (color == "green")
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Blue;
+
+
+            }
+
+            else if (color == "yellow")
+            {
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.White;
+
+            }
+
+            else
+            {
+                Console.WriteLine($"Ваш любимый цвет {color}, увы, в наши планы он не входил, поэтому используем дефолтный цвет :)");
+            }
+
+
+
+        }
+    }
+}
