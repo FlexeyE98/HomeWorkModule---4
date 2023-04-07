ab7f04702b02669d4410ffe4533186a1fcee5df5
Alexey Emelchikov <aleksemelchikov@gmail.com>
2023-04-07 15:51:37 +0300

    Итоговый практикум модуль 4

---
+++ HomeWorkModule4__finished/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace HomeWorkModule4__finished
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            (string name, string lastname, string login, int lengthLogin, bool isHavePet, int age, string[] colors) anketa;
+
+            anketa.colors = new string[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+
+                Console.Write("Введите имя: ");
+                anketa.Item1 = Console.ReadLine();
+                Console.Write("Введите фамилия: ");
+                anketa.Item2 = Console.ReadLine();
+                Console.Write("Введите логин: ");
+                anketa.Item3 = Console.ReadLine();
+                anketa.Item4 = anketa.Item3.Length;
+                Console.WriteLine($"Длина вашего логина: {anketa.Item4}");
+                Console.Write("Есть ли у вас домашнее животное ? (Напишите Да или Нет): ");
+                string havePet = Console.ReadLine();
+
+                if (havePet == "Да")
+                    anketa.isHavePet = true;
+                else
+                {
+                    anketa.isHavePet = false;
+
+                }
+                Console.WriteLine(anketa.isHavePet);
+
+                Console.Write("Введите ваш возраст: ");
+                anketa.Item6 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите три любимых цвета: ");
+                for (int k = 0; k < anketa.colors.Length; k++)
+                {
+                    string color = Console.ReadLine();
+                    anketa.colors[k] = color;
+
+
+                }
+                Console.WriteLine();
+
+                foreach (var x in anketa.colors)
+                { Console.Write($"Ваши любимые цвета: {x}\n"); }
+
+                Console.WriteLine();
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
--- HomeWorkModule4_unit_4.1.18/Program.cs
+++ HomeWorkModule4_unit_4.1.18/Program.cs
@@ -1,42 +1,53 @@
-﻿namespace HomeWorkModule4_unit_4._1._18
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
+
+namespace HomeWorkModule4_unit_4._1._18
 {
     internal class Program
     {
         static void Main(string[] args)
+
         {
 
-            Console.Write("Введите ваш любимый цвет: ");
-            string color = Console.ReadLine();
+            (string name, string lastname, string login, int lengthLogin, bool isHavePet, int age, string[] colors) anketa;
 
-            switch (color)
+            anketa.colors = new string[3];
+
+            Console.Write("Введите имя: ");
+            anketa.Item1 = Console.ReadLine();
+            Console.Write("Введите фамилия: ");
+            anketa.Item2 = Console.ReadLine();
+            Console.Write("Введите логин: ");
+            anketa.Item3 = Console.ReadLine();
+            anketa.Item4 = anketa.Item3.Length;
+            Console.WriteLine($"Длина вашего логина: {anketa.Item4}");
+            Console.Write("Есть ли у вас домашнее животное ? (Напишите Да или Нет): ");
+            string havePet = Console.ReadLine();
+
+            if (havePet == "Да")
+                anketa.isHavePet = true;
+            else
             {
-                case "red":
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    break;
-                case "green":
-                    Console.BackgroundColor = ConsoleColor.Green;
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                    break;
-                case "yellow":
-                    Console.BackgroundColor = ConsoleColor.White;
-                    Console.ForegroundColor = ConsoleColor.Cyan;
-                    break;
-                case "cyan":
-                    Console.BackgroundColor = ConsoleColor.Cyan;
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    break;
-                default:
-                    Console.BackgroundColor = ConsoleColor.Yellow;
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    break;
-
-
+                anketa.isHavePet = false;
 
             }
+            Console.WriteLine(anketa.isHavePet);
 
+            Console.Write("Введите ваш возраст: ");
+            anketa.Item6 = int.Parse(Console.ReadLine());
 
+            Console.WriteLine("Введите три любимых цвета: ");
+            for (int i = 0; i < anketa.colors.Length; i++)
+            {
+                string color = Console.ReadLine();
+                anketa.colors[i] = color;
+            }
+            Console.WriteLine();
 
+            foreach (var x in anketa.colors)
+                Console.Write($"Ваши любимые цвета: {x}\n");
 
         }
     }
